1a0cbbdcc3ba524b874784b5f5e40866eee3ab1c
Kirill Sidorchuk <kirill.sidorchuk@teamdev.com>
2013-02-28 16:06:33 +0200

    FilterPolicy interface added, Options class has now new reference to FilterPolicy

---
+++ leveldb-api/src/main/java/org/iq80/leveldb/FilterPolicy.java
@@ -0,0 +1,11 @@
+package org.iq80.leveldb;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kirill
+ * Date: 09.02.13
+ * Time: 16:08
+ * To change this template use File | Settings | File Templates.
+ */
+public interface FilterPolicy {
+}
